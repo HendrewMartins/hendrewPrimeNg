a11eeeee8d53e4413928bf2e3d75081591e4bd6c
HendrewMartins <hendrewmartins@hotmail.com>
2021-09-09 18:29:14 -0300

    Ultima tentativa

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/hendrewPrimeNg'),
+      dir: require('path').join(__dirname, './coverage/hendrewprimeng'),
       subdir: '.',
       reporters: [
         { type: 'html' },
