c5b3e418a12c73fc28dac27b3183f717c58517ce
HendrewMartins <hendrewmartins@hotmail.com>
2021-11-10 23:14:50 -0300

    Import Documents

--- src/app/modules/cadastros/importar/importar.component.ts
+++ src/app/modules/cadastros/importar/importar.component.ts
@@ -1,5 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 
 @Component({
@@ -8,13 +12,20 @@ import { MessageService } from 'primeng/api';
   styleUrls: ['./importar.component.css'],
   providers: [MessageService]
 })
-export class ImportarComponent implements OnInit {
+export class ImportarComponent<T> implements OnInit {
 
   public carregar: boolean = true;
   public pesquisaCar: boolean = true;
   public uploadedFiles: any[] = [];
+  public url: string;
+  public validProgress: boolean = false;
 
-  constructor(private messageService: MessageService) { }
+
+  constructor(private messageService: MessageService,
+    public http: HttpClient) {
+    this.url = environment.api + '/api/alunos';
+  }
+
 
   ngOnInit(): void {
     this.timeCarregar();
@@ -23,15 +34,66 @@ export class ImportarComponent implements OnInit {
   public timeCarregar() {
     setTimeout(() => {
       this.carregar = false;
-  }, 1500); 
+    }, 1500);
   }
 
   public onUpload(event: any) {
-    for(let file of event.files) {
-        this.uploadedFiles.push(file);
+    console.log('carregou');
+    for (let file of event.files) {
+      this.uploadedFiles.push(file);
     }
-    
-    this.messageService.add({severity: 'info', summary: 'Success', detail: 'Arquivo Carregado'});
-}
 
+    this.messageService.add({ severity: 'info', summary: 'Success', detail: 'Arquivo Carregado' });
+  }
+
+  public onFileUpload(files: any): void {
+    this.validProgress = true;
+    console.log(files.files[0]);
+    const formData: FormData = new FormData();
+    const file = files.files[0];
+
+    console.log(file);
+    console.log(file.name);
+    formData.append('file', file, file.name);
+    console.log(formData);
+
+    this.criarImport(formData).subscribe((registro) => {
+      // FIXME: Realizar ação após gravar o registro
+      this.validProgress = false;
+      alert(`O registro foi importado com sucesso!`);
+
+    }, error => {
+      console.error(error);
+      this.validProgress = false;
+      alert('Ocorreu um erro ao importar o registro');
+    });
+    
+
+  }
+
+  public myUploader(event: any) {
+    const formData: FormData = new FormData();
+    formData.append('file', event, event.name);
+    for (let file of event.files) {
+      this.uploadedFiles.push(file);
+    }
+    console.log(formData);
+    this.criarImport(this.uploadedFiles).subscribe((registro) => {
+      // FIXME: Realizar ação após gravar o registro
+      alert(`O registro foi importado com sucesso!`);
+
+    }, error => {
+      console.error(error);
+      alert('Ocorreu um erro ao importar o registro');
+    });
+  }
+
+
+
+  public criarImport(obj: any): Observable<T[]> {
+    return this.http.post<any>(this.url + '/importar', obj).pipe(map((item: any[]) => {
+      console.log(item);
+      return item;
+    }));
+  }
 }
